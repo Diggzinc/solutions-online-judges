75cc8b285b890ab4c1f749793c9eec921afdc4ef
Diogo Pinho <diogo.pinho@celfinet.com>
2016-06-09 13:25:57 +0100

    Added explanation for hackerrank solution manasa-and-stones

--- HackerRank/Challenges/manasa-and-stones.cpp
+++ HackerRank/Challenges/manasa-and-stones.cpp
@@ -5,6 +5,21 @@
 #include <algorithm>
 using namespace std;
 
+/*
+Commutative property: When two numbers are added, the sum is the same regardless of the order of the addends. 
+For example 4 + 2 = 2 + 4
+So a + b = b + a
+Which means that we only need the number of a's and b's and we can disregard their position to achieve the final sum
+
+example:
+N = 5 (the first stone is always 0)
+0 + a + a + a + a
+0 + a + a + a + b
+0 + a + a + b + b
+0 + a + b + b + b
+0 + b + b + b + b
+
+*/
 
 int main() { 
     int nCases, n, a, b;
